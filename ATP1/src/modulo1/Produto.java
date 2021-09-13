7359634ce54a8c9fe346c159398d18b262ed01d6
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-09-13 15:19:02 -0300

    criando ATP1

---
+++ ATP1/src/modulo1/Produto.java
@@ -0,0 +1,27 @@
+package modulo1;
+
+import java.util.Scanner;
+
+public class Produto {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("-------------------- Cadastro de Produtos -------------------\n");
+        System.out.println("\tBem-vindo(a), digite as informações para cadastrar um produto.");
+
+        System.out.print("\tDigite o nome do produto: ");
+        String nome = sc.next();
+
+        System.out.print("\tDigite a descrição do produto: ");
+        String descricao = sc.next();
+
+        System.out.print("\tDigite o valor do produto: ");
+        double valor = sc.nextDouble();
+
+        System.out.print("\tDigite a categoria do produto: ");
+        String categoria = sc.next();
+
+        System.out.printf("\tO nome do produto %s, %s, o valor é R$ %.2f, a categoria é %s.ge",nome, descricao, valor, categoria);
+
+    }
+}
