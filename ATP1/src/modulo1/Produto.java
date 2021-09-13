a39dd1319b0ab539a9e13dddc44828923467c7e8
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-09-13 16:22:31 -0300

    adicionando nextLine para pegar + de 1 palavra

--- ATP1/src/modulo1/Produto.java
+++ ATP1/src/modulo1/Produto.java
@@ -10,16 +10,16 @@ public class Produto {
         System.out.println("\tBem-vindo(a), digite as informações para cadastrar um produto.");
 
         System.out.print("\tDigite o nome do produto: ");
-        String nome = sc.next();
+        String nome = sc.nextLine();
 
         System.out.print("\tDigite a descrição do produto: ");
-        String descricao = sc.next();
+        String descricao = sc.nextLine();
+
+        System.out.print("\tDigite a categoria do produto: ");
+        String categoria = sc.nextLine();
 
         System.out.print("\tDigite o valor do produto: ");
         double valor = sc.nextDouble();
-
-        System.out.print("\tDigite a categoria do produto: ");
-        String categoria = sc.next();
 
         System.out.printf("\tO nome do produto %s: %s, o valor é R$ %.2f, a categoria é %s.",nome, descricao, valor, categoria);
 
