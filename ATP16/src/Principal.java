f305012d68b8af4a890a365b30bacea1a7b91db9
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-09-30 13:09:25 -0300

    construindo um menu

--- ATP16/src/Principal.java
+++ ATP16/src/Principal.java
@@ -6,22 +6,24 @@ e execute os métodos de transferência e de saque.
 _ Ao final da execução do método main, imprima todos os dados das taxas cobradas. 
 */
 public class Principal {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         CalculadoraTaxas taxas = new CalculadoraTaxas();
-        
-        imprimeMenu();
-        lerNumeroInt("Digite a opção desejada: ");
+        int opcao;
 
-        System.out.println("O saldo atual é: " + taxas.saldo());
-        taxas.depositar(1000);
-        System.out.println("O saldo atual é: " + taxas.saldo());
-        taxas.sacar(200);
-        System.out.println("O saldo atual é: " + taxas.saldo());
-        taxas.transferir(500);
-        System.out.println("O saldo atual é: " + taxas.saldo());       
-
+        do {
+            imprimeMenu();
+            opcao = lerNumeroInt("Digite a opção desejada: ");
+            
+            if(validaOpcao(opcao)){
+                menu(taxas, opcao);
+            }else{
+                System.out.println("Opção inválida!");
+            }
+            
+        } while ( !(validaOpcao(opcao) && opcao == 5));
     }
-    static void imprimeMenu(){
+
+    static void imprimeMenu() {
         System.out.println("Opções do Menu: ");
         System.out.println("\t 1 - Saldo");
         System.out.println("\t 2 - Sacar");
@@ -29,11 +31,58 @@ public class Principal {
         System.out.println("\t 4 - Tranferir");
         System.out.println("\t 5 - Sair");
     }
-    static int lerNumeroInt(String mensagem){
+
+    static int lerNumeroInt(String mensagem) {
         Scanner scan = new Scanner(System.in);
         System.out.print(mensagem);
         int numero = Integer.parseInt(scan.nextLine());
         return numero;
     }
-    
+
+    static double lerNumeroDouble(String mensagem) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print(mensagem);
+        double numero = Double.parseDouble(scan.nextLine());
+        return numero;
+    }
+
+    static void menu(CalculadoraTaxas taxas, int opcao){
+        double valor;
+
+        switch (opcao) {
+            case 1:
+                System.out.println("Você escolheu a opção vizualizar Saldo.");
+                System.out.println("O valor do saldo é: R$ " + taxas.saldo());    
+                break;
+            case 2:
+                System.out.println("Você escolheu a opção sacar.");
+                valor = lerNumeroDouble("Digite o valor em R$ para realizar a operação: ");
+                taxas.sacar(valor);
+                break;
+            case 3:
+                System.out.println("Você escolheu a opção depositar.");
+                valor = lerNumeroDouble("Digite o valor em R$ para realizar a operação: ");
+                taxas.depositar(valor);
+                break;
+            case 4:
+                System.out.println("Você escolheu a opção transferir.");
+                valor = lerNumeroDouble("Digite o valor em R$ para realizar a operação: ");
+                taxas.transferir(valor);
+                break;
+            case 5:
+                System.out.println("Você está saindo do menu. Obrigado!");
+                break;
+
+        }
+
+    }
+
+    static boolean validaOpcao(int opcao){
+        if (opcao < 1 || opcao > 5){;
+            return false;
+        }
+        
+        return true; 
+    }
+
 }
