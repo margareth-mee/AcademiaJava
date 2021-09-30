2519e25d6f453ad2d7c5865752baf584a0d030eb
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-09-30 11:46:19 -0300

    classe CalculadoraTaxas

---
+++ ATP16/src/CalculadoraTaxas.java
@@ -0,0 +1,11 @@
+/*
+_ Construa uma classe CalculadoraTaxas que possua os métodos de cálculo de taxas.
+_ Crie dois métodos para cálculo de taxa sobre transações bancárias, 
+o primeiro será aplicado uma taxa de 0,001% sobre o valor transferido, 
+o segundo uma taxa de saque de R$1.30 a cada 5 saques realizados.
+*/
+
+public class CalculadoraTaxas{
+    private double saldo;
+
+}
