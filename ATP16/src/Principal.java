0277110ddca6653e12a3477a94c97c15ca180d2b
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-09-30 12:13:09 -0300

    metodo lerNumeroInt

--- ATP16/src/Principal.java
+++ ATP16/src/Principal.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
 _ Na classe principal, dentro do metodo main, construa um objeto da classe Calculadora de Taxas 
 e execute os métodos de transferência e de saque.
@@ -6,15 +8,24 @@ _ Ao final da execução do método main, imprima todos os dados das taxas cobra
 public class Principal {
     public static void main(String[] args) {
         CalculadoraTaxas taxas = new CalculadoraTaxas();
-
+        
+        lerNumeroInt("Digite a opção desejada: ");
+        
         System.out.println("O saldo atual é: " + taxas.saldo());
         taxas.depositar(1000);
         System.out.println("O saldo atual é: " + taxas.saldo());
         taxas.sacar(200);
         System.out.println("O saldo atual é: " + taxas.saldo());
         taxas.transferir(500);
-        System.out.println("O saldo atual é: " + taxas.saldo());
+        System.out.println("O saldo atual é: " + taxas.saldo());       
 
+    }
+
+    static int lerNumeroInt(String mensagem){
+        Scanner scan = new Scanner(System.in);
+        System.out.print(mensagem);
+        int numero = Integer.parseInt(scan.nextLine());
+        return numero;
     }
     
 }
