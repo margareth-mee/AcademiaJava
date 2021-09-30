8a92704094d201fad202e4882d4bb299d5f10239
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-09-30 11:47:23 -0300

    métodos CalculadoraTaxas

--- ATP16/src/CalculadoraTaxas.java
+++ ATP16/src/CalculadoraTaxas.java
@@ -7,5 +7,22 @@ o segundo uma taxa de saque de R$1.30 a cada 5 saques realizados.
 
 public class CalculadoraTaxas{
     private double saldo;
+    
+    public void depositar(double valor){
+        saldo += valor; 
+    }
+
+    public void sacar(double valor){
+        saldo -= valor;
+    }
+
+    public double saldo(){
+        return saldo;
+    }
+
+    public void transferir(double valor){
+        saldo -= valor;
+    }
+    
 
 }
