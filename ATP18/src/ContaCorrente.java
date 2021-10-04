d658e98538f83277df4b81224e969dc0c368ef40
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-04 13:42:09 -0300

    gerando getters e setters de ContaCorrente

--- ATP18/src/ContaCorrente.java
+++ ATP18/src/ContaCorrente.java
@@ -3,4 +3,37 @@ public class ContaCorrente {
    private int numero;
    private int codigoCliente; 
    private double saldo; 
+
+   public void setAgencia(String agencia) {
+       this.agencia = agencia;
+   }
+
+   public String getAgencia() {
+       return this.agencia;
+   }
+
+   public void setNumero(int numero) {
+       this.numero = numero;
+   }
+
+   public int getNumero() {
+       return this.numero;
+   }
+
+   public void setCodigoCliente(int codigoCliente) {
+       this.codigoCliente = codigoCliente;
+   }
+
+   public int getCodigoCliente() {
+       return this.codigoCliente;
+   }
+
+   public void setSaldo(double saldo) {
+       this.saldo = saldo;
+   }
+
+   public double getSaldo() {
+       return this.saldo;
+   }
+
 }
