75017d4d9a62035994200ac1d68d6cae821ec496
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-04 17:26:39 -0300

    criando classe Numeros

---
+++ ATP19/src/Numeros.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Numeros {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arrayInteiros = new int[10];
+
+        for (int i = 0; i < arrayInteiros.length; i++) {
+            System.out.printf("Digite o valor do %dº número inteiro: ", i+1);
+            arrayInteiros[i] = Integer.parseInt(sc.nextLine());
+        }
+        for (int j : arrayInteiros) {
+            System.out.printf("valor: %d\n", j);
+            
+        }
+
+    }
+}
