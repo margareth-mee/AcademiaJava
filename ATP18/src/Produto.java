42158bd0641b1546d33d173d869911b9e26da759
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-04 13:32:41 -0300

    gerando getters e setters

--- ATP18/src/Produto.java
+++ ATP18/src/Produto.java
@@ -2,5 +2,37 @@ public class Produto {
     private int codigo;
     private String nome;
     private String descricao;
-    private int valor; 
+    private double valor; 
+
+    public void setCodigo(int codigo){
+        this.codigo = codigo;
+    }
+
+    public int getCodigo(){
+        return this.codigo;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+    public String getDescricaco(){
+        return this.descricao;
+    }
+
+    public void setValor(double valor){
+        this.valor = valor;
+    }
+
+    public double getValor(){
+        return this.valor;
+    }
+
 }
