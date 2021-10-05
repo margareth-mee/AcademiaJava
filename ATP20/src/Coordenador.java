058034f5c3ce59e55e486383533d1d9baff5c192
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-05 15:29:27 -0300

    criando classe coordenador

---
+++ ATP20/src/Coordenador.java
@@ -0,0 +1,17 @@
+public class Coordenador {
+    private String curso_coordenado;
+    private double bonus;
+
+    public void setCurso_coordenado(String curso_coordenado) {
+        this.curso_coordenado = curso_coordenado;
+    }
+    public String getCurso_coordenado() {
+        return curso_coordenado;
+    }
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+    public double getBonus() {
+        return bonus;
+    }
+}
