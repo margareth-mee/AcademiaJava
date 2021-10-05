1e3d42313ee8db816f822bf70ff17b02e1f227ca
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-05 15:50:09 -0300

    usando setters e getters das classes

--- ATP20/src/Universidade.java
+++ ATP20/src/Universidade.java
@@ -3,6 +3,42 @@ public class Universidade {
         Funcionario funcionario = new Funcionario();
         Aluno aluno = new Aluno();
         Coordenador coordenador = new Coordenador();
-        
+
+        funcionario.setNome("Bruna");
+        funcionario.setSobrenome("Bittencourt");
+        funcionario.setMatricula(777);
+        funcionario.setDepartamento("Engenharia");
+        funcionario.setSalario(10000);
+
+        coordenador.setNome("Maykon");
+        coordenador.setSobrenome("Granneman");
+        coordenador.setMatricula(778);
+        coordenador.setCurso_coordenado("Ciencias da Computação");
+        coordenador.setBonus(5000);
+
+        aluno.setNome("Meg");
+        aluno.setSobrenome("Lima");
+        aluno.setMatricula(114110571);
+        aluno.setCurso("Engenharia Elétrica");
+        aluno.setTurma("2014.1");
+
+        System.out.println("------------ Funcionário ----------------");
+        System.out.println("Nome: " + funcionario.getNome() + " " + funcionario.getSobrenome());
+        System.out.println("Matrícula: " + funcionario.getMatricula());
+        System.out.println("Departamento: " + funcionario.getDepartamento());
+        System.out.println("Salário: " + funcionario.getSalario());
+
+        System.out.println("------------ Coordenador ----------------");
+        System.out.println("Nome: " + coordenador.getNome() + " " + coordenador.getSobrenome());
+        System.out.println("Matrícula: " + coordenador.getMatricula());
+        System.out.println("Curso Coordenado: " + coordenador.getCurso_coordenado());
+        System.out.println("Bonus: " + coordenador.getBonus());
+
+        System.out.println("------------ Aluno ----------------");
+        System.out.println("Nome: " + aluno.getNome() + " " + aluno.getSobrenome());
+        System.out.println("Matrícula: " + aluno.getMatricula());
+        System.out.println("Curso: " + aluno.getCurso());
+        System.out.println("Turma: " + aluno.getTurma());
+
     }
 }
