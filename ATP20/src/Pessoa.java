d7b24d566e630d784ba7cf68b4969780f3dc2185
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-05 15:25:49 -0300

    criando classe Pessoa

---
+++ ATP20/src/Pessoa.java
@@ -0,0 +1,25 @@
+public class Pessoa{
+
+    private String nome;
+    private String sobrenome;
+    private int matricula;
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    public String getSobrenome() {
+        return sobrenome;
+    }
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+    public int getMatricula() {
+        return matricula;
+    }
+}
