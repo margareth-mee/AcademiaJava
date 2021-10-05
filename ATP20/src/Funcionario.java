7f3524d2be92ad889ec62bcd833d592243f9247e
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-05 15:27:50 -0300

    criando classe Funcionario

---
+++ ATP20/src/Funcionario.java
@@ -0,0 +1,17 @@
+public class Funcionario {
+    private String departamento;
+    private double salario;
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+    public String getDepartamento() {
+        return departamento;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    public double getSalario() {
+        return salario;
+    }
+}
