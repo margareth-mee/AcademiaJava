3dfa59f98b86524dcd75cb7c679b8f2aa9d84a0b
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-05 15:35:35 -0300

    cirnaod classe Universidade

---
+++ ATP20/src/Universidade.java
@@ -0,0 +1,8 @@
+public class Universidade {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+        Aluno aluno = new Aluno();
+        Coordenador coordenador = new Coordenador();
+        
+    }
+}
