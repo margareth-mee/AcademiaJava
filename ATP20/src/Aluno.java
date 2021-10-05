9a7bbddafd2118c0cdbdd1c9ab31d8fba8c04bd1
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-05 15:30:50 -0300

    criando classe Aluno

---
+++ ATP20/src/Aluno.java
@@ -0,0 +1,17 @@
+public class Aluno {
+    private String curso;
+    private String turma;
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+    public String getCurso() {
+        return curso;
+    }
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+    public String getTurma() {
+        return turma;
+    }
+}
