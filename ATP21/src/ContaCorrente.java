5ac8c4336b17f1aa49790c39369f5b2728b8ad26
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-06 13:15:16 -0300

    criando classe ContaCorrente

---
+++ ATP21/src/ContaCorrente.java
@@ -0,0 +1,7 @@
+/*_ Crie uma classe Conta Corrente com os atributos privados: taxa transferência, taxa manutenção e número de transferências.*/
+public class ContaCorrente {
+    private double taxa_transferencia;
+    private double taxa_manuntencao;
+    private int numero_transferencias;
+    
+}
