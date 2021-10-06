c4f8fcec8ebfae6cbb60bae2252498e6d018a56a
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-06 14:00:58 -0300

    override do metodo transferencia

--- ATP21/src/ContaCorrente.java
+++ ATP21/src/ContaCorrente.java
@@ -1,8 +1,18 @@
 /*_ Crie uma classe Conta Corrente com os atributos privados: taxa transferência, taxa manutenção e número de transferências.*/
 public class ContaCorrente extends Conta{
     private double taxa_transferencia = 10.50;
-    private double taxa_manuntencao = 2.5;
+    private double taxa_manutencao = 2.5;
     private int numero_transferencias = 0;
 
-    
+    @Override
+    public void transferencia(double valor){
+        
+        super.transferencia(valor);
+        setSaldo(getSaldo() - taxa_transferencia);
+        numero_transferencias++;
+        
+        if(numero_transferencias % 5 == 0){
+            setSaldo(getSaldo() - taxa_manutencao);
+        }
+    }
 }
--- ATP21/src/Teste.java
+++ ATP21/src/Teste.java
@@ -3,7 +3,7 @@ public class Teste {
         Conta conta = new Conta();
         ContaCorrente conta_corrente = new ContaCorrente();
 
-        /*------------- Cadastrando Cliente do tipo Conta ------------------------*/
+        System.err.println("------ Cadastrando Cliente do tipo Conta ----------");
         conta.setCodigo_cliente("114110571");
         conta.setSaldo(2500.00);
 
@@ -20,22 +20,23 @@ public class Teste {
         System.out.println("Código Cliente: " + conta.getCodigo_cliente());
         System.out.println("Saldo: " + conta.getSaldo());
 
-        /*------------- Cadastrando Cliente do tipo ContaCorrente ------------------*/
+        System.out.println("\n------- Cadastrando Cliente do tipo ContaCorrente ------");
         conta_corrente.setCodigo_cliente("114110576");
         conta_corrente.setSaldo(4500.00);
 
         //imprindo informações da conta
-        System.out.println("Código Cliente: " + conta_corrente.getCodigo_cliente());
+        System.out.println("\nCódigo Cliente: " + conta_corrente.getCodigo_cliente());
         System.out.println("Saldo: " + conta_corrente.getSaldo());
 
-        //realizando transferencia
-        double transferencia2 = 500;
-        conta_corrente.transferencia(transferencia2);
-        System.out.printf("Uma transferência de %.2f foi realizada!\n", transferencia2);
-       
-        //imprimindo informações da conta após transferência
-        System.out.println("Código Cliente: " + conta_corrente.getCodigo_cliente());
-        System.out.println("Saldo: " + conta_corrente.getSaldo());
-        
+        double transferencia2 = 50;
+        for (int i = 1; i <= 10; i++) {
+            //realizando transferencia
+            conta_corrente.transferencia(transferencia2);
+            System.out.printf("\nOperação nº %d: uma transferência de %.2f foi realizada!\n", i, transferencia2);
+ 
+            //imprimindo informações da conta após transferência
+            System.out.println("Código Cliente: " + conta_corrente.getCodigo_cliente());
+            System.out.println("Saldo: " + conta_corrente.getSaldo());
+        }                
     }
 }
