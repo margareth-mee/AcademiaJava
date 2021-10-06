f5486417652fc132075edde6905a4332e754854d
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-06 13:13:05 -0300

    criando classe Conta

---
+++ ATP21/src/Conta.java
@@ -0,0 +1,18 @@
+public class Conta{
+    private double saldo;
+    private String codigo_cliente;
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setCodigo_cliente(String codigo_cliente) {
+        this.codigo_cliente = codigo_cliente;
+    }
+    public String getCodigo_cliente() {
+        return codigo_cliente;
+    }
+    
+}
