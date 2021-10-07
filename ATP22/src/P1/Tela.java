77b859390c6304cb8e9439a7bce61510355ea2c1
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-07 15:57:33 -0300

    criando classe Tela

---
+++ ATP22/src/P1/Tela.java
@@ -0,0 +1,18 @@
+package P1;
+
+public class Tela {
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno();
+        Aluno aluno2 = new Aluno(); 
+
+        aluno1.setNome_completo("Maykon Granemann");
+        aluno1.setIdade(18);
+        aluno1.setMatricula("114110571");
+        
+        aluno2.setNome_completo("Cintia Maria");
+        aluno2.setIdade(30);
+        aluno2.setMatricula("114110577");
+
+    }
+    
+}
