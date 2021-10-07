47ff524609f4b529094e6c8403b7720efa5fc9b5
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-07 16:10:26 -0300

    método saque

--- ATP22/src/P2/ContaCorrente.java
+++ ATP22/src/P2/ContaCorrente.java
@@ -2,12 +2,15 @@ package P2;
 
 public class ContaCorrente {
     private double saldo;
-    private double taxa_saque;
+    private double taxa_saque = 10.0;
     private double codigo_cliente;
 
     public ContaCorrente(double saldo, double codigo_cliente){
         this.saldo = saldo;
         this.codigo_cliente = codigo_cliente;
+    }
+    public void saque(double valor){
+        this.saldo -= valor + taxa_saque;
     }
         
 }
