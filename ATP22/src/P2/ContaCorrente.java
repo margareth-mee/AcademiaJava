7baef88d431625e68cc1150b698c545fbf09addf
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-07 16:08:49 -0300

    criando classe ContaCorrente

---
+++ ATP22/src/P2/ContaCorrente.java
@@ -0,0 +1,13 @@
+package P2;
+
+public class ContaCorrente {
+    private double saldo;
+    private double taxa_saque;
+    private double codigo_cliente;
+
+    public ContaCorrente(double saldo, double codigo_cliente){
+        this.saldo = saldo;
+        this.codigo_cliente = codigo_cliente;
+    }
+        
+}
