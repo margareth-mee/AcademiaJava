33a456c8e74ead442c2cf71da404e4c8ba5ce474
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-18 14:16:03 -0300

    colocando arquivos em scr

---
+++ ATP24/src/Carro.java
@@ -0,0 +1,6 @@
+package src;
+public class Carro {
+    public int num_portas;
+    public double num_litros_porta_malas;
+
+}
---
+++ ATP24/src/Veiculo.java
@@ -0,0 +1,7 @@
+package src;
+public class Veiculo{
+    public String marca;
+    public String modelo;
+    public String placa;
+
+}
