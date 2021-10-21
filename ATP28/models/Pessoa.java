56dd007aa0e59bf4d3bf71be3b55ed2a94a2f563
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-21 15:14:09 -0300

    Pessoa's constructor

--- ATP28/models/Pessoa.java
+++ ATP28/models/Pessoa.java
@@ -5,6 +5,10 @@ public class Pessoa{
     public String id;
     public Endereco endereco; 
 
+    public Pessoa(){
+        endereco = new Endereco();
+    }
+
     @Override
     public boolean equals(Object obj) {
 
@@ -17,5 +21,10 @@ public class Pessoa{
         }
        
         return false;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + this.id + "\nNome completo: " + this.nomeCompleto + "\nEndereço: " + this.endereco.rua + ", nº " + this.endereco.numero + "\nCidade: " + this.endereco.cidade + "\nCEP: " + this.endereco.CEP; 
     }
 }
