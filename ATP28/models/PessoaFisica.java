0d5269f6dc9f85ce4fbd6f269a280e03f6f0b946
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-21 15:13:13 -0300

    overrind of toString method

--- ATP28/models/PessoaFisica.java
+++ ATP28/models/PessoaFisica.java
@@ -2,4 +2,9 @@ package models;
 
 public class PessoaFisica extends Pessoa {
     public String CPF;
+    
+    @Override
+    public String toString() {
+        return super.toString() + "\nCPF: " + CPF;
+    }
 }
--- ATP28/models/PessoaJuridica.java
+++ ATP28/models/PessoaJuridica.java
@@ -2,4 +2,9 @@ package models;
 
 public class PessoaJuridica extends Pessoa{
     public String CNPJ;
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nCNPJ: " + this.CNPJ;
+    }
 }
