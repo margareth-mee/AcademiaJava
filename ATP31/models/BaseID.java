765b5ada01f6c56f70809f5c9bd58c971420ebef
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-10-26 10:00:16 -0300

    BaseID equals

--- ATP31/models/BaseID.java
+++ ATP31/models/BaseID.java
@@ -2,4 +2,16 @@ package models;
 
 public class BaseID {
     public String id;
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof BaseID){
+            BaseID base = (BaseID) obj;
+
+            if(this.id.equals(base.id)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- ATP31/models/Produto.java
+++ ATP31/models/Produto.java
@@ -4,6 +4,10 @@ public class Produto extends BaseID{
     public String nome;
     public Categoria categoria;
 
+    public Produto(){
+        categoria = new Categoria();
+    }
+
     @Override
     public String toString() {
         return "Produto ID: " + this.id + "\nNome: " + this.nome + this.categoria.toString();
