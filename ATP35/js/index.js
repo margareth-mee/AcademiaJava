704ca7d27e016256f175faee96c1e570f2ed07f5
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-11-03 09:49:29 -0300

    atividade atp35

---
+++ ATP35/js/index.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+    console.log('Loading JS...'); 
+    let frame = document.getElementById("frame");
+    frame.src="welcome.html"
+}
+
+function resumo(){
+    let frame = document.getElementById("frame");
+    frame.src="resumo.html"
+}
+function experiencias(){
+    let frame = document.getElementById("frame");
+    frame.src="experiencias.html"
+}
+function habilidades(){
+    let frame = document.getElementById("frame");
+    frame.src="habilidades.html"
+}
+function contatos(){
+    let frame = document.getElementById("frame");
+    frame.src="contatos.html"
+}
