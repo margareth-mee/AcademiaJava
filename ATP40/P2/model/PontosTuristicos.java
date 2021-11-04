0b290677219788f1a499a79c8e1dba01ecaccff6
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-11-04 18:02:33 -0300

    criando classe PontosTuristicos

---
+++ ATP40/P2/model/PontosTuristicos.java
@@ -0,0 +1,29 @@
+package P2.model;
+/* 
+_  Crie um sistema para cadastro de Pontos Turísticos. A classe de modelo deve possuir três atributos públicos: nome, descrição e localização.
+_  Crie uma interface que contenha os métodos de leitura e gravação de dados para pontos turísticos.
+_  Crie duas implementações da interface. Uma que usará um lista para armazenar os dados e outra que utilizará um arquivo texto como fonte de dados.
+_  Crie duas classes View para testar cada uma das implementações criadas. */
+
+public class PontosTuristicos {
+    public String nome;
+    public String descricao;
+    public String localizacao;
+
+    public PontosTuristicos(String linha){
+        String[] dados = linha.split(";");
+        this.nome= dados[0];
+        this.descricao = dados[1];
+        this.localizacao = dados[2];
+    }
+
+    @Override
+    public String toString() {
+        String retorno = String.format("%s;%s;%s", 
+        this.nome, 
+        this.descricao,
+        this.localizacao);
+        
+    return retorno;
+    }
+}
