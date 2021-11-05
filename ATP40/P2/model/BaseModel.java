84d8fe39ddd04818af29ee33217e62b54a1ca625
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-11-05 10:13:47 -0300

    Base model

---
+++ ATP40/P2/model/BaseModel.java
@@ -0,0 +1,20 @@
+package P2.model;
+
+public class BaseModel {
+    public int id;
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof BaseModel){
+            BaseModel outro = (BaseModel)obj;
+            if(outro.id == this.id){
+                return true;
+            }
+        }
+        return false;
+    }
+    @Override
+    public String toString() {       
+        return Integer.toString(this.id);
+    }
+}
