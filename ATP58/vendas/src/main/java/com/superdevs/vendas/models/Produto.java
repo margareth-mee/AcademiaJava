f16b8a54638625ab49607d757cc01654aec6f871
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-11-24 17:50:29 -0300

    ATP58 - deletar e alterar

---
+++ ATP58/vendas/src/main/java/com/superdevs/vendas/models/Produto.java
@@ -0,0 +1,26 @@
+package com.superdevs.vendas.models;
+
+public class Produto {
+    private String nome;
+    private float valor;
+    private int id_categoria;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public float getValor() {
+        return valor;
+    }
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+    public int getId_categoria() {
+        return id_categoria;
+    }
+    public void setId_categoria(int id_categoria) {
+        this.id_categoria = id_categoria;
+    }
+}
---
+++ ATP58/vendas/src/main/java/com/superdevs/vendas/servlets/CategoriaDeletarServlet.java
@@ -0,0 +1,29 @@
+package com.superdevs.vendas.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import com.superdevs.vendas.dao.CategoriaDao;
+import com.superdevs.vendas.models.Categoria;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/categoria/deletar")
+public class CategoriaDeletarServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        CategoriaDao dao = new CategoriaDao();
+        Categoria model = new Categoria();
+
+        int id = Integer.parseInt(req.getParameter("id"));
+        model.setId(id);
+        dao.delete(model);
+
+        PrintWriter pw = resp.getWriter();
+        pw.printf("catetegoria id = %d deletado", id);
+    }
+}
